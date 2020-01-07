a9aed702f159f0750104ccba502b8064ae975a0b
Brad Wright <bradwrightdiscord@gmail.com>
2020-01-07 20:18:27 +0000

    updates and added cooldown to punch to save resources

--- src/commands/Interactive/Punch.js
+++ src/commands/Interactive/Punch.js
@@ -1,5 +1,6 @@
 const { Command } = require('klasa');
 const { MessageEmbed } = require('discord.js');
+const { punch: gifs } = require('../../../lib/util/constants');
 module.exports = class extends Command {
 
     constructor(...args) {
@@ -9,23 +10,19 @@ module.exports = class extends Command {
             requiredPermission: ['SEND_MESSAGES', 'EMBED_MESSAGES'],
             description: language => language.get('COMMAND_PUNCH_DESCRIPTION'),
             usage: '<user:username>',
+            bucket: 1,
+            cooldown: 20,
+            cooldownLevel: 'author',
             usageDelim: ' ',
         });
         this.customizeResponse('user', 'are u actually ok? a user is required')
     }
 
     async run(message, [user]) {
-        let gifs = [
-          'https://i.imgur.com/oLCiTRD.gif',
-          'https://i.imgur.com/ppjt8rO.gif',
-          'https://i.imgur.com/lgpfjk2.gif',
-          'https://i.imgur.com/WqqGDNt.gif'
-        ]
         let randomgif = Math.floor((Math.random() * gifs.length));
-        return message.send(`${message.author} jus smacked the shit out of ${user}`, {
-          embed: new MessageEmbed()
-          .setColor("#36393f")
-          .setImage(gifs[randomgif])
+        return message.channel.sendFile(gifs[randomgif])
+        .then(msg => {
+          msg.edit(`${message.author} jus smacked the shit out of ${user}`)
         })
     }
 };
