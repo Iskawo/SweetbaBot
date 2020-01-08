58308c7d1ec1c90eafeada42c3cc56c815e65cb0
Brad Wright <bradwrightdiscord@gmail.com>
2020-01-08 19:23:26 +0000

    sdobhguiob

--- src/events/ready.js
+++ src/events/ready.js
@@ -10,7 +10,7 @@ module.exports = class extends Event {
       this.client.user.setPresence({
         status: 'online',
         activity: {
-          name: 'daddy',
+          name: 'daddy 🤤',
           type: 'WATCHING'
         }
      });
