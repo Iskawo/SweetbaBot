d2a36d10162b377624e6211a0a2d7d382b7be76d
Brad Wright <bradwrightdiscord@gmail.com>
2020-01-08 17:38:30 +0000

    updated git command

--- src/commands/Secret/Git.js
+++ src/commands/Secret/Git.js
@@ -6,53 +6,19 @@ module.exports = class extends Command {
         super(...args, {
             name: 'git',
             runIn: ['text', 'dm'],
-            subcommands: true,
             permissionLevel: 10,
             requiredPermission: ['SEND_MESSAGES', 'EMBED_MESSAGES'],
             description: 'Execute a terminal command',
-            usage: '<add|push|pull|commit|status> [flag:string] [cm:...string]',
+            usage: '<command:...string>',
             usageDelim: ' ',
             extendedHelp: ' '
         });
-        this.customizeResponse('push', `Invalid options. Use either of: \`add, push, pull, commit, status\``);
+        this.customizeResponse('command', `Provide a git command to run.`);
     }
 
-    async add(msg, [cm]) {
+    async run(msg, [command]) {
       try {
-          let executed = await execSync(`git add ${cm}`).toString();
-          return msg.channel.send(`\`\`\`js\n${executed}\`\`\``);
-      } catch (err) {
-          return msg.channel.send(`\`\`\`js\n${err}\`\`\``);
-      }
-    }
-    async push(msg) {
-      try {
-          let executed = await execSync(`git push`).toString();
-          return msg.channel.send(`\`\`\`js\n${executed}\`\`\``);
-      } catch (err) {
-          return msg.channel.send(`\`\`\`js\n${err}\`\`\``);
-      }
-    }
-    async pull(msg) {
-      try {
-          let executed = await execSync(`git pull`).toString();
-          return msg.channel.send(`\`\`\`js\n${executed}\`\`\``);
-      } catch (err) {
-          return msg.channel.send(`\`\`\`js\n${err}\`\`\``);
-      }
-    }
-    async commit(msg, [flag, cm]) {
-      if (!cm) return msg.send(`Provide a commit message`);
-      try {
-          let executed = await execSync(`git commit ${flag} ${cm}`).toString();
-          return msg.channel.send(`\`\`\`js\n${executed}\`\`\``);
-      } catch (err) {
-          return msg.channel.send(`\`\`\`js\n${err}\`\`\``);
-      }
-    }
-    async status(msg) {
-      try {
-          let executed = await execSync(`git status`).toString();
+          let executed = await execSync(`git ${command}`).toString();
           return msg.channel.send(`\`\`\`js\n${executed}\`\`\``);
       } catch (err) {
           return msg.channel.send(`\`\`\`js\n${err}\`\`\``);
